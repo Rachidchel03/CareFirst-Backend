281733724c87403566136bbd38fe1e31a7d9e4e3
Rachid003 <an0n.3m@hotmail.com>
2025-05-29 20:35:40 +0200

    change ports

--- main.py
+++ main.py
@@ -146,5 +146,5 @@ def download_excel(timestamp: str):
 
 if __name__ == "__main__":
     import uvicorn
-    port =os.getenv("PORT", 8000)
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    
+    uvicorn.run(app, host="0.0.0.0", port=10000)
